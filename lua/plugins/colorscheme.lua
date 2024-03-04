4680372534163d91b8142d3b1374e99c1dcfe8a8
Heverton <hb.hevertonborges@gmail.com>
2024-03-04 19:11:41 -0300

    current settings

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      disable_background = true,
+      disable_float_background = true,
+    },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      background_colour = "#1a1b26",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
